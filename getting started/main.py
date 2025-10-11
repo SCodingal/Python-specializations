5ede32a3055fd90d2c3d07264c539f79e2cbac02
SCodingal <tchiynamelo11@gmail.com>
2025-10-11 13:20:28 +0100

    Add files via upload

---
+++ getting started/main.py
@@ -0,0 +1,33 @@
+#Print a sentence
+print("Welcome to the world of programming!")
+#print a number
+print(4)
+#next line after print
+print("Hello python \n")
+#pass multiple agreements for printing
+print("hello", 5)
+#end agreement of print statment
+print("Welcome to" , end= "*")
+
+# Creating diffrent variables 
+x = 5
+y="John"
+print(x)
+print(y)
+codingal="codingal"
+print(codingal)
+#User input
+name= input("enter your name:")
+print("\nHello", name, "\nWelcome to codingal")
+
+# keywords
+import keyword
+#print all the python keywords
+print("Python keywords are ...\n")
+print(keyword.kwlist)
+
+
+name= input("Hello, enter your name:")
+grade= input("Which grade are you in?:")
+school= input("Whats the name of your school?:")
+print("\nWelcome to codingal", name, "/n your grade is" , grade, "/n Your schools name is" , school,)
