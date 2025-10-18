4347c7215b02616622425d69926ae1e1cfb6a207
SCodingal <tchiynamelo11@gmail.com>
2025-10-18 13:28:37 +0100

    Add files via upload

---
+++ Pyton operations/main.py
@@ -0,0 +1,31 @@
+# Arethmetic operations
+num1= 6
+num2= 7
+print("addition" , (num1+num2))
+print("subtraction" , (num1-num2))
+print("multiplication" , (num1*num2))
+print("division" , (num1/num2))
+print("module" , (num1%num2))
+print("floor division" , (num1//num2))
+print("exponent" , (num1**num2))
+# Conditional operators
+print("grater than" , num1>num2)
+print("less than" , (num1<num2))
+print("grater or equals to" , (num1>=num2))
+print("less than or equals to" , (num1<=num2))
+print("not equals to" , (num1!=num2))
+print("equals to" , (num1==num2))
+# Assignment operators
+num1+=3
+print(num1)
+num1**=2
+print(num1)
+# average
+m1= 50
+m2= 49
+m3= 48
+m4= 47
+average=(m1+m2+m3+m4)/4
+print("average", average)
+percentage=(m1+m2+m3+m4)/200
+print("percentage" , percentage*100)
