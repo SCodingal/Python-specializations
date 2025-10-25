7674c6b2b67f951b2a3bb9b2ebdb8d295a9c8888
SCodingal <tchiynamelo11@gmail.com>
2025-10-25 16:12:19 +0100

    seasons

---
+++ seasons/main.py
@@ -0,0 +1,14 @@
+weather= str(input("Enter the wather are your place:"))
+s1="winter"
+s2="summer"
+s3="spring"
+s4="autmn"
+if(weather=s1):
+    print("You must wear: Puffy coat, winter boats, scarf and a warm head set")
+elif(weather=s2):
+    print("You must wear: shorts or legging, open hole shoes like snadels or flipflops and shirts with short sleves")
+elif(weather=s3):
+        print("You must wear: loafer or sandels legging and bolouses ")
+elif(weather=s4):
+         print("You must wear: Boots, cardigans and coats for warmth")
+
