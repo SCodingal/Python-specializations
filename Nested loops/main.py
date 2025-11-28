c5511d1377cfcb891e35bfc8ce88b14f115a1544
SCodingal <tchiynamelo11@gmail.com>
2025-11-28 17:39:17 +0100

    Add files via upload

---
+++ Nested loops/main.py
@@ -0,0 +1,23 @@
+string= input("Please enter your own word:")
+char=input("Please enter your own character: ")
+
+i = 0
+count = 0
+while(i< len(string)):
+    if (string[i] == char):
+        count = count + 1
+    i=i+1
+print("The total Number of Times", char, "has Occured=" , count)
+
+lower = int(input("Enter a lower range:"))
+upper= int(input("Enter a upper range:"))
+print("Prime numbesr between", lower, "and" ,upper, "are:")
+for num in range(lower, upper+1):
+    if num>1:
+      for i in range(2,num):
+         if (num%i) ==0:
+            break
+      else:
+         print(num)
+
+
