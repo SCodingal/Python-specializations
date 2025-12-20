8887377b339aad36d6b7fc438384d9b9eda1e42a
SCodingal <tchiynamelo11@gmail.com>
2025-12-20 13:26:11 +0100

    Add files via upload

---
+++ Exception handelling/main.py
@@ -0,0 +1,26 @@
+try:
+    number = int(input("Enter a number: "))
+    print("The number entered is" , number)
+except ValueError as ex:
+    print("Exception:" , ex)
+print("End of the program")
+
+try:
+    num1, num2 = eval(input("Enter two numbers, separated by a comma: "))
+    result = num1 / num2
+    print("Result is" , result)
+
+except ZeroDivisionError:
+    print("Division by zero is error !!")
+
+except SyntaxError:
+    print("Comma is missing. Enter numbers separated by comma like this 1, 2")
+
+except:
+    print("Wrong input")
+else:
+    print("No exceptions")
+
+finally:
+    print("This will execute no matter what")
+
