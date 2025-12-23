0ed515b7f9a0668e4928ccb1e580f1fa2d4d7336
SCodingal <tchiynamelo11@gmail.com>
2025-12-23 20:16:19 +0100

    Add files via upload

---
+++ age counter/main.py
@@ -0,0 +1,17 @@
+valid = False
+while not valid:
+    try:
+        n=int(input("Enter your age: "))
+    if n== 2,4,6,8,10:
+        print("Well your age is even")
+    else:
+        print("nope sorry, age is odd")
+    
+    except ValueError:
+        print("Invalid")
+
+
+    
+    
+
+    
