8b34f93d7cf9212c86b61f6ded308b4e4654b1ff
SCodingal <tchiynamelo11@gmail.com>
2025-12-26 18:24:10 +0100

    Add files via upload

---
+++ shutdown/main.py
@@ -0,0 +1,9 @@
+c= str(input("Do you want to procide with the shutdown or your action? "))
+
+if c== yes:
+   print("Window is shuting down")
+   break
+   
+else:
+   print("You can continue with ur work, ill be here when you want to")
+   
