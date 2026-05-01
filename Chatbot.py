9566cf018e95d4435ece165e4542f9f84c37d049
SCodingal <tchiynamelo11@gmail.com>
2026-05-01 21:05:24 +0100

    Add files via upload

---
+++ Chatbot.py
@@ -0,0 +1,29 @@
+print("Hello! I am AI Bot 300. What's your name friend? : ")
+
+name=input()
+
+print(f"Plesure to meet you{name}!")
+
+print("How are you feeling today?(good/bad/other):")
+mood= input().lower()
+
+if mood == "good":
+    print("I'm glad your feeling in such a jolly way!")
+
+elif mood == "bad":
+    print("I'm so sorry to hear that, hope you feel better soon")
+
+elif mood =="other":
+    print("It's okay, hope you have a nice day though!")
+else:
+    print("I see it don't worry. Words can be hard to let out")
+
+print(" Hasve any hobbies? waht are they share with me!")
+hobbies=input().lower
+if hobbies ==" any":
+    print(" wow thats amazing! Your so athletic")
+else:
+    print("Aww thats okay, I don't do any sports or something similar too")
+
+
+print(f"It was a nice chatting with you{name}. Goodbye! ")
